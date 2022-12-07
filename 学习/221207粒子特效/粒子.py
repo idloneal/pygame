ccd6103d65e49870c8bfd6020dd8c00c35ed694c
idloneal <haoyue9526@gmail.com>
2022-12-07 23:01:09 +0800

    ...

--- 学习/221207粒子特效/粒子.py
+++ 学习/221207粒子特效/粒子.py
@@ -30,6 +30,34 @@ class ParticlePrinciple():
         particle_copy = [particle for particle in self.particles if particle[1]>0]  # 三目运算
         self.particles = particle_copy
 
+class ParticleNyan():
+    def __init__(self):
+        self.particles = []
+        self.size = 8
+
+    def emit(self):  # 移动并创建粒子
+        if self.particles:
+            self.delete_particles()
+            for particle in self.particles:
+                particle[0][1] += particle[2][0]  # 移动
+                particle[0][0] += particle[2][1]
+                particle[1] -= 0.2  # 缩小他的半径
+                pygame.draw.circle(screen,pygame.Color('White'),particle[0], int(particle[1]))
+
+    def add_particles(self):
+        pos_x = pygame.mouse.get_pos()[0]
+        pos_y = pygame.mouse.get_pos()[1]
+        radius = 10
+        direction_x = random.randint(-3,3)
+        direction_y = random.randint(-3,3)
+        particle_circle = [[pos_x,pos_y],radius,[direction_x,direction_y]]  # 给他们一些属性
+        self.particles.append(particle_circle)
+
+    def delete_particles(self):
+        # 删除半径小于0的圆
+        particle_copy = [particle for particle in self.particles if particle[1]>0]  # 三目运算
+        self.particles = particle_copy
+
 
 
 pygame.init()
@@ -39,8 +67,10 @@ clock = pygame.time.Clock()
 
 particle1 = ParticlePrinciple()
 
+nyan_surface = pygame.image.load('nyan_cat.jpg').convert_alpha()
+
 PARTICLE_EVENT = pygame.USEREVENT + 1
-pygame.time.set_timer(PARTICLE_EVENT,150)
+pygame.time.set_timer(PARTICLE_EVENT,50)
 
 while True:
     for event in pygame.event.get():
