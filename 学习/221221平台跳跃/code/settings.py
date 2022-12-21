27cac9f34dd3ef901d82f2eef53501b5dd92c39b
idloneal <haoyue9526@gmail.com>
2022-12-21 21:54:22 +0800

    初具雏形

---
+++ 学习/221221平台跳跃/code/settings.py
@@ -0,0 +1,15 @@
+level_map = [
+    '                            ',
+    '                            ',
+    '                            ',
+    ' XX    XXX            XX    ',
+    ' XX P                       ',
+    ' XXXX        XX          XX ',
+    ' XXXX       XX              ',
+    ' XX    X  XXXX    XX  XX    ',
+    '       X  XXXX    XX  XXX   ',
+    '    XXXX  XXXXXX  XX  XXXX  ',
+    'XXXXXXXX  XXXXXX  XX  XXXX  ']
+
+tile_size = 64
+bg_size = (1200, len(level_map) * 64)
