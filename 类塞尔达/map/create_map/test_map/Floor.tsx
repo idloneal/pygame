c2440f380962f187136288665dccc765ba6a91b1
idloneal <haoyue9526@gmail.com>
2023-01-15 22:02:07 +0800

    add map、floor、details、grass、Object

---
+++ 类塞尔达/map/create_map/test_map/Floor.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Floor" tilewidth="64" tileheight="64" tilecount="550" columns="22">
+ <image source="../../../graphics/tilemap/Floor.png" width="1408" height="1600"/>
+ <wangsets>
+  <wangset name="未命名集" type="mixed" tile="-1"/>
+ </wangsets>
+</tileset>
